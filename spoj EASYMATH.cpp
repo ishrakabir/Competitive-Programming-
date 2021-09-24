e06f2004d03b6e6909734962e0f9ce0a3533a503
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-09-24 17:31:51 +0600

    Add files via upload

---
+++ spoj EASYMATH.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned ll;
+using ld  = long double;
+using pi  = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi  = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI           3.141592653589793238
+#define w( n )       while( n-- )
+#define fir          first
+#define sec          second
+#define all(x)       (x).begin(),(x).end()
+#define rall(x)      (x).rbegin(),(x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define loop(i,n,x)  for(int i=n;i<x;i++)
+#define sz(a)        ((int) (a).size())
+#define st(x)        sort( x.begin(), x.end() )
+#define For(n,x)     for(int i=1;i<=x;i++)
+#define ite          iterator it
+#define fast         ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define test         ll t;cin>>t; while(t--){solve();}
+#define in           insert
+#define el           endl
+
+void InputOutput()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("G:/Code/input.txt", "r", stdin);
+    freopen("G:/Code/output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+
+long long GCD( long long num1, long long num2 ) 
+{
+    long long temp;
+ 
+    while( num2 != 0 )
+    {
+        temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+ 
+    return num1;
+}
+
+
+long long LCM( long long num1, long long num2 )
+{
+    long long gcd, lcm;
+ 
+    gcd = GCD( num1, num2 );
+ 
+    lcm = ( num1 * num2 ) / gcd;
+ 
+    return lcm;
+}
+
+void solve()
+{
+    ll t;cin>>t;
+    w(t)
+    {
+
+    ll n,m,a,d;
+    cin>>n>>m>>a>>d;
+    vector<ll>v;
+    v.push_back(a);
+    v.push_back(a+d);
+    v.push_back(a+(2*d));
+    v.push_back(a+(3*d));
+    v.push_back(a+(4*d));
+    // for(int i=0;i<v.size();i++)
+    //     cout<<v[i]<<" ";
+    // cout<<endl;
+   ll len=v.size();
+   ll sum=0;
+   
+    loop(mask,1,pow(2,len))
+    {
+        ll count=0,prod=1;
+        loop(i,0,len)
+        {
+            if(mask & (1<<i))
+                {
+                    count++;
+                    prod=LCM(prod,v[i]);
+                }
+        }
+        if(count%2==0)
+                    sum-=((m / prod) - ((n-1) / prod));
+                else
+                    sum+=((m / prod) - ((n-1) / prod));
+                // cout<<sum<<" ";
+    }
+    ll x=m-n+1;
+    cout<<x-sum<<endl;
+
+ }
+
+
+}
+
+
+
+
+int main()
+{
+
+
+    fast;
+    InputOutput();
+     solve();
+//     test;
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr <<el<< "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
+
+
