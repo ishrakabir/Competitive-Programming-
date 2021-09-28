9b657b3c470295ffc75a041a94c64341f699ac25
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-09-29 01:33:58 +0600

    Create CF1579-D3-A.cpp

---
+++ CF1579-D3-A.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned ll;
+using ld  = long double;
+using pi  = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi  = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI           3.141592653589793238
+#define w( n )       while( n-- )
+#define fir          first
+#define sec          second
+#define all(x)       (x).begin(),(x).end()
+#define rall(x)      (x).rbegin(),(x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define loop(i,n,x)  for(int i=n;i<x;i++)
+#define sz(a)        ((int) (a).size())
+#define st(x)        sort( x.begin(), x.end() )
+#define For(n,x)     for(int i=1;i<=x;i++)
+#define ite          iterator it
+#define fast         ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define test         ll t;cin>>t; while(t--){solve();}
+#define in           insert
+#define el           endl
+
+void InputOutput()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("G:/Code/input.txt", "r", stdin);
+    freopen("G:/Code/output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+
+void solve()
+{
+
+  int t;cin>>t;
+  w(t)
+  {
+    string str;
+    cin>>str;
+    int A=0,B=0,C=0;
+     for(int i=0;i<str.size();i++)
+     {
+        if(str[i]=='A')
+            A++;
+        else if(str[i]=='B')
+            B++;
+        else
+            C++;
+     }
+     // cout<<A<<" "<<B<<" "<<C<<" ";
+     if(B==0)
+        cout<<"no"<<endl;
+    else if(A!=0&&B!=0&&C!=0)
+    {
+        B-=A;
+        C-=B;
+        if(C==0)
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+    }
+    else if(C==0&&A!=0&&B!=0)
+    {
+        if(A==B)
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+    }
+    else if(A==0&&B!=0&&C!=0)
+    {
+        if(B==C)
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+    }
+    else if(A==B==C)
+        cout<<"no"<<endl;
+  }
+
+}
+
+
+
+int main()
+{
+
+
+    fast;
+    InputOutput();
+     solve();
+//     test;
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr <<el<< "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
