268643d49e4a93b7c3f22e3cd5a896bdd32f5ae6
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-10-26 14:44:19 +0600

    Add files via upload

---
+++ Codeforces 1204A BowWow and the Timetable.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned ll;
+using ld  = long double;
+using pi  = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi  = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI           3.141592653589793238
+#define w( n )       while( n-- )
+#define fir          first
+#define sec          second
+#define all(x)       (x).begin(),(x).end()
+#define rall(x)      (x).rbegin(),(x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define loop(i,n,x)  for(int i=n;i<x;i++)
+#define sz(a)        ((int) (a).size())
+#define st(x)        sort( x.begin(), x.end() )
+#define For(n,x)     for(int i=1;i<=x;i++)
+#define ite          iterator it
+#define fast         ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define test         ll t;cin>>t; while(t--){solve();}
+#define in           insert
+#define el           endl
+
+void InputOutput()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("G:/Code/input.txt", "r", stdin);
+    freopen("G:/Code/output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+ll binaryToDecimal(string n)
+{
+    string num = n;
+    ll dec_value = 0;
+ 
+    ll base = 1;
+ 
+    ll len = num.length();
+    for (ll i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+ 
+    return dec_value;
+}
+
+
+void solve()
+{
+
+	string num;
+	cin>>num;
+	int count=0;
+	int flag=0;
+	for(int i=0;i<num.size();i++)
+	{
+		if(num[i]=='1')
+			count++;
+		if(count>=2)
+			{
+				flag=1;
+				break;
+			}
+	}
+	if(flag==1)
+	{
+		cout<<ceil((double(num.size()))/2)<<endl;
+	}
+	else
+		cout<<num.size()/2<<endl;
+
+
+}
+
+
+
+int main()
+{
+
+
+    fast;
+    InputOutput();
+     solve();
+//     test;
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr <<el<< "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
+
+
