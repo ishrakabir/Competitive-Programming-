d44b70b5b6717b542f9ff69d8fd974f3ff5e4ec3
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-10-27 12:07:49 +0600

    Add files via upload

---
+++ Codeforces Edu D Fast search.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned ll;
+using ld  = long double;
+using pi  = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi  = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI           3.141592653589793238
+#define w( n )       while( n-- )
+#define fir          first
+#define sec          second
+#define all(x)       (x).begin(),(x).end()
+#define rall(x)      (x).rbegin(),(x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define loop(i,n,x)  for(int i=n;i<x;i++)
+#define sz(a)        ((int) (a).size())
+#define st(x)        sort( x.begin(), x.end() )
+#define For(n,x)     for(int i=1;i<=x;i++)
+#define ite          iterator it
+#define fast         ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define test         ll t;cin>>t; while(t--){solve();}
+#define in           insert
+#define el           endl
+
+void InputOutput()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("G:/Code/input.txt", "r", stdin);
+    freopen("G:/Code/output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+
+void solve()
+{
+
+	int n;
+	cin>>n;
+	int arr[n];
+	loop(i,0,n)
+	cin>>arr[i];
+	sort(arr,arr+n);
+	int q;
+	cin>>q;
+	w(q)
+	{
+		int x,y;
+		cin>>x>>y;
+		int l1=-1,r1=n;
+		while(l1+1<r1)
+		{
+			int m=(l1+r1)/2;
+			if(x<=arr[m])r1=m;
+			else
+				l1=m;
+		}
+
+		int l2=-1,r2=n;
+		while(l2+1<r2)
+		{
+			int m=(l2+r2)/2;
+			if(y<arr[m])r2=m;
+			else
+				l2=m;
+		}
+		cout<<abs(r2-r1)<<endl;
+	}
+
+}
+
+
+
+int main()
+{
+
+
+    fast;
+    InputOutput();
+     solve();
+//     test;
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr <<el<< "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
+
+
