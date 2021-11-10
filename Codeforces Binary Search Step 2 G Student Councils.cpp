304c3f34017d1af1773f8f2ea8804904c7898985
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-11-10 15:17:27 +0600

    Add files via upload

---
+++ Codeforces Binary Search Step 2 G Student Councils.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned ll;
+using ld  = long double;
+using pi  = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi  = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI           3.141592653589793238
+#define w( n )       while( n-- )
+#define fir          first
+#define sec          second
+#define all(x)       (x).begin(),(x).end()
+#define rall(x)      (x).rbegin(),(x).rend()
+#define pb           push_back
+#define mp           make_pair
+#define loop(i,n,x)  for(int i=n;i<x;i++)
+#define sz(a)        ((int) (a).size())
+#define st(x)        sort( x.begin(), x.end() )
+#define For(n,x)     for(int i=1;i<=x;i++)
+#define ite          iterator it
+#define fast         ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define test         ll t;cin>>t; while(t--){solve();}
+#define in           insert
+#define el           endl
+
+void InputOutput()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("G:/Code/input.txt", "r", stdin);
+    freopen("G:/Code/output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int k, n;
+bool good(vector<ll> v, ll m) {
+    ll sum = 0;
+    for(int i=0;i<v.size();i++)
+    	sum+=min(m,v[i]);  
+
+    return sum >= m * k;
+}
+
+void solve()
+{
+
+	
+    cin >> k >> n;
+
+    vector<ll> v(n);
+    for(int i=0;i<n;i++)
+    	cin>>v[i];
+
+    ll l = 0;
+    ll r = 1e17;
+    while (r > l + 1)
+     {
+        ll m = (l + r) / 2;
+        if (good(v, m)) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+
+    cout << l << endl;
+
+}
+
+
+
+int main()
+{
+
+
+    fast;
+    InputOutput();
+     solve();
+//     test;
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr <<el<< "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
+
+
