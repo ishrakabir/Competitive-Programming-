59d83ac8470d7fb2008d93245700e09f4b586194
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-11-12 20:33:26 +0600

    Add files via upload

---
+++ A_Di_visible_Confusion.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using pi = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI 3.141592653589793238
+#define w(n) while (n--)
+#define fir first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define loop(i, n, x) for (int i = n; i < x; i++)
+#define sz(a) ((int)(a).size())
+#define st(x) sort(x.begin(), x.end())
+#define For(n, x) for (int i = 1; i <= x; i++)
+#define test     \
+    ll t;        \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+
+#define ite iterator it
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define in insert
+#define el endl
+
+void solve()
+{
+
+    ll n, x = 0;
+    cin >> n;
+    ll arr[n];
+    bool flag = 0;
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+    for (int i = 1; i <= n; i++)
+    {
+        if (arr[i] % (i + 1) != 0)
+            x++;
+        else
+        {
+            flag = 0;
+            for (int j = 1; j <= x; j++)
+            {
+                if (arr[i] % ((i + 1) - j) != 0)
+                {
+                    x++;
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                cout << "NO" << el;
+                return;
+            }
+        }
+    }
+    cout << "YES" << el;
+}
+
+int main()
+{
+
+    fast;
+    // solve();
+    test;
+    return 0;
+}
