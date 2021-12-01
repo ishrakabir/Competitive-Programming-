1a6b9415f2ae8cd0c110fc343664786ee233ceee
ishrakabir <64137066+ishrakabir@users.noreply.github.com>
2021-12-01 09:31:53 +0600

    Add files via upload

---
+++ A_Make_Even.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using pi = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI 3.141592653589793238
+#define w(n) while (n--)
+#define fir first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define loop(i, n, x) for (int i = n; i < x; i++)
+#define sz(a) ((int)(a).size())
+#define st(x) sort(x.begin(), x.end())
+#define For(n, x) for (int i = 1; i <= x; i++)
+#define ite iterator it
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define test     \
+    ll t;        \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+#define in insert
+#define el endl
+
+void solve()
+{
+
+    int t;
+    cin >> t;
+    w(t)
+    {
+        int k;
+        cin >> k;
+        if (k % 2 == 0)
+        {
+            cout << 0 << el;
+        }
+        else
+        {
+            stringstream ss;
+            ss << k;
+            string s;
+            ss >> s;
+            int count = 0;
+            for (int i = 0; i < s.size(); i++)
+            {
+                int p = s[i] - '0';
+                if (p % 2 == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0)
+            {
+                cout << -1 << el;
+            }
+            else
+            {
+                int p = s[0] - '0';
+                if (p % 2 == 0)
+                    cout << 1 << el;
+                else
+                    cout << 2 << el;
+            }
+        }
+    }
+}
+
+int main()
+{
+    fast;
+    solve();
+    //     test;
+}
