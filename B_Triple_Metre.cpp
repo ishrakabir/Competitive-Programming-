f3d7c2842c1750dcbc92dc4a813b20a354a10e90
ishrak_abir <64137066+ishrakabir@users.noreply.github.com>
2021-12-05 12:13:05 +0600

    Add files via upload

---
+++ B_Triple_Metre.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using pi = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI 3.141592653589793238
+#define w(n) while (n--)
+#define fir first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define loop(i, n, x) for (int i = n; i < x; i++)
+#define sz(a) ((int)(a).size())
+#define st(x) sort(x.begin(), x.end())
+#define For(n, x) for (int i = 1; i <= x; i++)
+#define ite iterator it
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define test     \
+    ll t;        \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+#define in insert
+#define el endl
+int isSubstring(string s1, string s2)
+{
+    int M = s1.length();
+    int N = s2.length();
+    for (int i = 0; i <= N - M; i++)
+    {
+        int j;
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j])
+                break;
+
+        if (j == M)
+            return i;
+    }
+
+    return -1;
+}
+void solve()
+{
+
+    string s1;
+    cin >> s1;
+    int n = s1.size();
+    string t = "oxx";
+    string s2 = "";
+    for (int i = 0; i < n; i++)
+        s2 += t;
+    int res = isSubstring(s1, s2);
+    if (res == -1)
+        cout << "No" << el;
+    else
+        cout << "Yes" << el;
+}
+
+int main()
+{
+    fast;
+    solve();
+    //     test;
+}
