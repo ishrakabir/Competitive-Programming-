776549f92c9650360a60abe387034b2fc526d9d4
ishrak_abir <64137066+ishrakabir@users.noreply.github.com>
2021-12-09 11:56:30 +0600

    Add files via upload

---
+++ A_Kalevitch_and_Chess.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using pi = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI 3.141592653589793238
+#define w(n) while (n--)
+#define fir first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define loop(i, n, x) for (int i = n; i < x; i++)
+#define sz(a) ((int)(a).size())
+#define st(x) sort(x.begin(), x.end())
+#define For(n, x) for (int i = 1; i <= x; i++)
+#define ite iterator it
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define test     \
+    ll t;        \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+#define in insert
+#define el endl
+
+void solve()
+{
+
+    int n = 8;
+    char c[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> c[i][j];
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (c[i][j] == 'B')
+                cnt++;
+        }
+        if (cnt == n)
+            count++;
+    }
+    int count2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (c[j][i] == 'B')
+                cnt++;
+        }
+        if (cnt == n)
+            count2++;
+    }
+    if (count + count2 == 16)
+        cout << 8 << el;
+    else
+        cout << count + count2 << el;
+}
+
+int main()
+{
+    fast;
+    solve();
+    //     test;
+}
