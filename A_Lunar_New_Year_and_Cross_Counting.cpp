47d405e2673bf20dd62888d682376d563cdad711
ishrak_abir <64137066+ishrakabir@users.noreply.github.com>
2021-12-10 23:15:50 +0600

    Add files via upload

---
+++ A_Lunar_New_Year_and_Cross_Counting.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using pi = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI 3.141592653589793238
+#define w(n) while (n--)
+#define fir first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define loop(i, n, x) for (int i = n; i < x; i++)
+#define sz(a) ((int)(a).size())
+#define st(x) sort(x.begin(), x.end())
+#define For(n, x) for (int i = 1; i <= x; i++)
+#define ite iterator it
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define test     \
+    ll t;        \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+#define in insert
+#define el endl
+
+void solve()
+{
+    int n, ans = 0;
+    string s[1001];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+    for (int i = 1; i < n - 1; i++)
+    {
+        for (int j = 1; j < n - 1; j++)
+        {
+            if (s[i][j] == 'X' && s[i - 1][j - 1] == 'X' && s[i + 1][j - 1] == 'X' && s[i + 1][j + 1] == 'X' && s[i - 1][j + 1] == 'X')
+            {
+                ans++;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+    solve();
+    //     test;
+}
