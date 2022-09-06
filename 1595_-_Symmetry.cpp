d74880f25a3dd1b5b81cceb14f07e32c4d0d9a67
ishrak_abir <64137066+ishrakabir@users.noreply.github.com>
2022-09-06 17:56:39 +0600

    Add files via upload

---
+++ 1595_-_Symmetry.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned ll;
+using ld = long double;
+using pi = pair<int, int>;
+using pii = pair<ll, ll>;
+using vi = vector<int>;
+using vii = vector<ll>;
+using vip = vector<pair<int, int>>;
+
+const int MAX_N = 1e5 + 1;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+#define ci cin
+#define co cout
+
+#define PI 3.141592653589793238
+#define w(n) while (n--)
+#define fir first
+#define sec second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define loop(i, n, x) for (int i = n; i < x; i++)
+#define sz(a) ((int)(a).size())
+#define st(x) sort(x.begin(), x.end())
+#define For(n, x) for (int i = 1; i <= x; i++)
+#define ite iterator it
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define test     \
+    ll t;        \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+#define in insert
+#define el endl
+
+void solve()
+{
+    int t;
+    cin >> t;
+    w(t)
+    {
+        int n;
+        cin >> n;
+        vector<pair<ll, ll>> v;
+        for (int i = 0; i < n;i++)
+        {
+            ll x, y;
+            cin >> x >> y;
+            v.push_back(make_pair(x*2, y));
+        }
+        sort(v.begin(), v.end());
+        vector<pair<ll, ll>> vl,vr;
+        int mid=v[0].first+(v[n-1].first-v[0].first)/2;
+        int min = v[0].first;
+        int max=v[n-1].first;
+        for (int i = 0; i < n;i++)
+        {
+            if(v[i].first<mid)
+            vl.push_back(make_pair(v[i].first-min,v[i].second));
+            else if(v[i].first>mid)
+            vr.push_back(make_pair(max-v[i].first,v[i].second));
+        }
+        sort(vl.begin(), vl.end());
+        sort(vr.begin(), vr.end());
+        if(vl==vr)
+            cout << "YES" << el;
+            else
+            cout<<"NO"<<el;
+    }
+}
+
+int main()
+{
+    fast;
+    solve();
+    //     test;
+}
