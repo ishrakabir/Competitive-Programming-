cf897c57702a17f1ea25f61c49c5aab5d4b96bc7
ishrak_abir <64137066+ishrakabir@users.noreply.github.com>
2022-11-19 16:01:59 +0600

    Update Spoj primes_triangle_I.cpp

--- Spoj primes_triangle_I.cpp
+++ Spoj primes_triangle_I.cpp
@@ -1,48 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-using ll = long long;
-using ull = unsigned ll;
-using ld = long double;
-using pi = pair<int, int>;
-using pii = pair<ll, ll>;
-using vi = vector<int>;
-using vii = vector<ll>;
-using vip = vector<pair<int, int>>;
-
-const int MAX_N = 1e5 + 1;
-const ll mod = 1e9 + 7;
-const ll INF = 1e9;
-
-#define ci cin
-#define ell cout << el << el
-#define co cout
-
-#define PI 3.141592653589793238
-#define w(n) while (n--)
-#define fir first
-#define sec second
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define mp make_pair
-#define loop(i, n, x) for (int i = n; i < x; i++)
-#define sz(a) ((int)(a).size())
-#define st(x) sort(x.begin(), x.end())
-#define For(n, x) for (int i = 1; i <= x; i++)
-#define ite iterator it
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-#define test     \
-    ll t;        \
-    cin >> t;    \
-    while (t--)  \
-    {            \
-        solve(); \
-    }
-#define in insert
-#define el endl
 int prime[100000000], nPrime;
 int mark[100000002];
 int arr2[100000002];
@@ -85,25 +42,18 @@ void sieve(int n)
         col++;
     }
 }
-void solve()
+int main()
 {
     sieve(100000000);
     int t;
     cin >> t;
-    w(t)
+    while (t--)
     {
         int n;
         cin >> n;
-        if(mark[n]==0)
-            cout << arr1[n] << " " << arr2[n] << el;
+        if (mark[n] == 0)
+            cout << arr1[n] << " " << arr2[n] << endl;
         else
-            cout << -1 << el;
+            cout << -1 << endl;
     }
-}
-
-int main()
-{
-    fast;
-    solve();
-    //     test;
 }
